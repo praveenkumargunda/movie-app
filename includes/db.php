3cfa87d01050ddded7f7e68e74c7c55bd4cc7a68
praveenkumargunda <108858306+praveenkumargunda@users.noreply.github.com>
2024-08-17 15:49:01 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,13 @@
-
 <?php
 require_once 'config.php';
 
 function getDbConnection() {
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT); // Added DB_PORT
+
     if ($link === false) {
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }
+
     return $link;
 }
 ?>
