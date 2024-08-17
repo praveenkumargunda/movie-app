e569560fb385bdaa6ec0d0de2436fb4974d199a6
praveenkumargunda <108858306+praveenkumargunda@users.noreply.github.com>
2024-08-17 15:19:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
-
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'movie_app');
+define('DB_PORT', '3310');  // Added line to define the new port
 
 // Replace 'your_omdb_api_key' with your actual OMDB API key
 define('OMDB_API_KEY', '618ee09c');
